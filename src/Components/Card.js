01b34d16175c4fa211ef8f8e75e53a5818f13e74
kayathrisivakumar <kayathrisivakumar@gmail.com>
2023-10-09 19:22:50 +0530

    first commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,13 +7,13 @@ const [btnText,setBtnText]=useState("Add to Cart")
 return(
 <React.Fragment>
 <div className="cardstyle">
-    <img src={image} alt="bag image"/>
+    <img src={image} alt="bag"/>
     
     <div><h2>{Name}</h2></div>
     <div>{cost}</div>
     <button onClick={()=>{    
-    btnText=="Add to Cart"?setBtnText("Remove from Cart"):setBtnText("Add to Cart") 
-    btnText=="Add to Cart"?setCart(cart+1):setCart(cart-1)   
+    btnText==="Add to Cart"?setBtnText("Remove from Cart"):setBtnText("Add to Cart") 
+    btnText==="Add to Cart"?setCart(cart+1):setCart(cart-1)   
     }}>{btnText}</button> 
 </div>
 </React.Fragment>
